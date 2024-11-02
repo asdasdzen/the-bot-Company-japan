c9bf77213140b42a6372b8f4cf4cd241a7c78e2a
asdasdzen <zzen54585@gmail.com>
2024-11-02 18:21:25 +0300

    the bot Company japan

---
+++ index.js
@@ -0,0 +1,17 @@
+let eeeeee
+
+
+
+document.getElementById('feedback').onclick = function(){
+  eeeeee = document.getElementById('feedback1').value;
+
+
+document.getElementById('ha').textContent = `(ありがとうございます フィードバックをお寄せください (${eeeeee}))`
+
+
+
+
+
+
+
+}
